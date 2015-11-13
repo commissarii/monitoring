76f10f28548ff8d4a17d040319282248aeddcd1b
commissarii <commissarii@mail.ru>
2015-11-13 14:15:36 +0300

    обновление 0.17

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 #coding=utf-8
 __author__ = "KomissarovAV"
-__version__ = "0.16"
-# программа разработа и протестирована при компиляторе python 3.4
 
 import datetime, sys, os, mysql.connector
 from mysql.connector import MySQLConnection, errorcode
 from python_mysql_dbconfig import read_db_config
 
-# объявление класса 'Таблица'
 class Table:
     def __init__(self, name, id_field):
         self.name = name
@@ -15,20 +12,20 @@ class Table:
 
     # функция по изменению значения столбца таблицы
     def ResetCounter(self):
-        print(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+        print(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
         min = Table.GetMin(self)
         max = Table.GetMAX(self)
         if (min == None):
-            print("Предупреждение: В столбце `{0}` таблицы `{1}` нет данных\n".format(table.id_field, table.name))
+            print('Предупреждение: В столбце `{0}` таблицы `{1}` нет данных\n'.format(table.id_field, table.name))
             return
         if (min == 1):
             print('Никаких изменений не требуется для столбца `{0}` в таблице `{1}`\n'
-                       ' Минимальное значение столбца = {2}\n Максимальное значение столбца = {3}'
-                       .format(table.id_field, table.name, min, max))
+                  ' Минимальное значение столбца = {2}\n Максимальное значение столбца = {3}'
+                  .format(table.id_field, table.name, min, max))
             return
         print('Было произведено изменение значений столбца `{0}` в таблице `{1}`\n'
-                   ' Минимальное значение столбца было = {2}\n Максимальное значение столбца было = {3}'
-                   .format(table.id_field, table.name, min, max))
+              ' Минимальное значение столбца было = {2}\n Максимальное значение столбца было = {3}'
+              .format(table.id_field, table.name, min, max))
         cursor.execute('Lock TABLES {0} WRITE;'.format(table.name))
         cursor.execute('UPDATE {0} SET {1} = {1} - {2} + 1'.format(table.name, table.id_field, min))
         cursor.execute('ALTER TABLE {0} AUTO_INCREMENT = 1'.format(table.name))
@@ -55,42 +52,42 @@ class Table:
             data = i
         return data
 
-# основная программа
 now_time = datetime.datetime.now()
-db_config = read_db_config()
-
 # создание файла-отчета в рабочем каталоге программы
+old_stdout = sys.stdout
 sys.stdout = open(os.getcwd()+'/logs.txt', 'w')
-print('Отчёт работы программы за {}\n      -       -       -       -       -       -       -       -       -    '
-           .format(now_time.strftime('%d.%m.%Y %I:%M %p')))
+print('Отчёт работы программы {}\n      -       -       -       -       -       -       -       -       -    '
+      .format(now_time.strftime('%d.%m.%Y %I:%M %p')))
 
 try:
+    db_config = read_db_config()
     cnx = MySQLConnection(**db_config)
-    if cnx.is_connected():
-        print('Подключение к базе данных прошло успешно')
-        cursor = cnx.cursor(buffered=True)
-        # массив таблиц и их столбцов в базе данных Monitoring
-        tables_list = [
-                        Table('CalculationTasks', 'id'), Table('EphemeridesGlonass', 'id'), Table('LoadedArchives', 'id'),
-                        Table("LoadedRinexFiles", "id"), Table("Messages", "message_id"), Table("SatellitesStatuses", "id"),
+    print('Подключение к базе данных прошло успешно')
+    cursor = cnx.cursor(buffered=True)
+    tables_list = [
+                        #Table('CalculationTasks', 'id'), Table('EphemeridesGlonass', 'id'), Table('LoadedArchives', 'id'),
+                        #Table("LoadedRinexFiles", "id"), Table("Messages", "message_id"), Table("SatellitesStatuses", "id"),
                         Table("SatFiles", "id"), Table("Stations", "id"), Table("StationsReliability", "id"),Table("UEREs", "id")
                       ]
-        for table in tables_list:
-            table.ResetCounter()
-        cursor.close()
-        cnx.disconnect()
-    else:
-        print('При подключении к базе данных произошла ошибка\n')
+    for table in tables_list:
+        table.ResetCounter()
+    cursor.close()
+    cnx.disconnect()
 
 except mysql.connector.Error as err:
-    print(' - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * -')
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print('Что-то не так с вашим именем пользователя или паролем')
+    print(' - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * - * -\n'
+          'Произошла ошибка:')
+    if err.errno == errorcode.ER_DBACCESS_DENIED_ERROR:
+        print('     Что-то не так с вашим именем пользователя')
+    elif err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('     Что-то не так с вашим паролем')
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print('Базы данных не существует')
+        print('     Что-то не так с название Базы Данных')
+    elif err.errno == errorcode.CR_CONN_HOST_ERROR:
+        print('     Что-то не так с IP-адрессом Базы Данных')
     else:
-        print('Произошла ошибка\nError code: {0}\nError message: {1}'
-                   .format(err.errno, err.msg))
+        print(' Error code: {0}\n Error message: {1}'.format(err.errno, err.msg))
 finally:
-    print("==========================================================================\n")
+    print('==========================================================================\n')
     sys.stdout.close()
+    sys.stdout = old_stdout
